baab71640c739984a96499168d1d2c6d491aa975
Altsome <ryan07command@gmail.com>
2024-02-08 10:18:30 +0900

    numpy pracitce

---
+++ numpy101.py
@@ -0,0 +1,7 @@
+import numpy as np
+from activation_functions.functions import *
+
+a = np.array([5, 4, 3, 2, 1])
+print(a[a % 2 == 0]) #boolean indexing
+
+print(step_funcion(np.array([1, 2, 3, -1])))
