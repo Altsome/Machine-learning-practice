2f82f3ab6e60f5d231aff4d11c3bffd485de63a3
Altsome <ryan07command@gmail.com>
2024-02-29 14:41:50 +0900

    modify functions for batch

--- network/mnist_network.py
+++ network/mnist_network.py
@@ -1,15 +1,15 @@
-import pickle
 import sys, os
-sys.path.append(os.pardir)  # 부모의 디렉터리에서 파일을 가져올 수 있도록 허용
 import numpy as np
-from PIL import Image
+import pickle
 from dataset.mnist import load_mnist
 from functions.activation_functions import softmax, sigmoid
+from PIL import Image
+sys.path.append(os.pardir)  # 부모의 디렉터리에서 파일을 가져올 수 있도록 허용
 
 
 def get_data():
     (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=True, one_hot_label=True)
-    # normalize = 정규화(0~255 -> 0~1), flatten = 1차원 배열로 폄
+    # normalize = 정규화(0~255 -> 0~1), flatten = 1차원 배열로 폄, one_hot_label = 4 -> [0, 0, 0, 0, 1, 0, 0, 0, 0, 0]
     return x_train, t_train, x_test, t_test
 
 
@@ -34,7 +34,7 @@ def forward(network, x):
     return y
 
 
-def mini_batch(batch_size):
+def get_batch(batch_size):
     train_size = x_train.shape[0]
     batch_mask = np.random.choice(train_size, batch_size)
     x_batch = x_train[batch_mask]
@@ -42,46 +42,31 @@ def mini_batch(batch_size):
     return x_batch, t_batch
 
 
+# forward 실행
 x_train, t_train, x_test, t_test = get_data()
-network = init_network()
-
-print(np.argmax(forward(network, x_test[1])))
-print(t_test[1])
-
-def evaluate_accuracy():
-    accuracy_count = 0
-    for i in range(len(x_test)):
-        p = np.argmax(forward(network, x_test[i]))
-        accuracy_count += (p == t_test[i])
-    return accuracy_count / len(x_test)
-
-
-def evaluate_accuracy_on_batch(batch_size):
-    accuracy_count = 0
-    for i in range(0, len(x_test), batch_size):
-        x_batch = x_test[i:i+batch_size]
-        t_batch = t_test[i:i+batch_size]
-        p = np.argmax(forward(network, x_batch), axis=1)
-        accuracy_count += (np.sum(p == t_batch))
-    return accuracy_count / len(x_test)
-
-print(evaluate_accuracy_on_batch(100))
+network = init_network()    # 네트워크 지정
+print(np.argmax(forward(network, x_test[1])))   # 예측값을 출력(np.argmax: one_hot_label을 숫자로 변환)
+print(np.argmax(t_test[1]))    # 정닶을 출력
 
 
 # 정확도 구하는 함수
-# def evaluate_accuracy(batch_size):
-#     accuracy_cnt = 0
-#     for i in range(0, len(x_test), batch_size):  # 데이터를 batch_size만큼씩 띄워서 자름
-#         x_batch = x_test[i:i+batch_size]     # 이미지들을 batch_size단위로 자름
-#         y_batch = forward(network, x_batch)
-#         p = np.argmax(y_batch, axis=1)    # 행에 대해 확률이 가장 높은 원소 = 추론한 정답을 가져옴
-#
-#         accuracy_cnt += np.sum(p == t_test[i:i+batch_size])  # 각 원소가 같은다는 조건의 boolean 리스트의 값들을 전부 합침
-#
-#     print("Accuracy:" + str(float(accuracy_cnt) / len(x_test)))
+def evaluate_accuracy(batch_size):
+    accuracy_cnt = 0
+    for i in range(0, len(x_test), batch_size):  # 데이터를 batch_size만큼씩 띄워서 자름
+        x_batch = x_test[i:i+batch_size]     # 이미지들을 batch_size단위로 자름
+        y_batch = forward(network, x_batch)
+        predict = np.argmax(y_batch, axis=1)    # 행에 대해 확률이 가장 높은 원소 = 추론한 정답을 가져옴
+        answer = np.argmax(t_test[i:i+batch_size], axis=1)
+
+        accuracy_cnt += np.sum(predict == answer)  # 각 원소가 같다는 조건의 boolean 리스트의 값들을 전부 합침
+
+    print("Accuracy:" + str(float(accuracy_cnt) / len(x_test)))
 
 
-# 이미지를 보여주는 함수
+evaluate_accuracy(100)
+
+
+# # 이거 작동 안됨
 # def img_show():
 #     x_test, t_test = get_data()
 #     print(t_test[0])
